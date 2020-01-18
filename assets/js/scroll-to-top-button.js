d32e1c69ba58e90268c59d7809c3bed5471dd457
JacOng17 <jacob_ong88@hotmail.com>
2020-01-19 00:52:15 +1300

    Edit scrollToTop btn & removed unused styles.

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -3,13 +3,13 @@
 // Get the button:
 var mybutton = document.getElementById("scrollToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 200px from the top of the document, show the button
 window.onscroll = function() {
   scrollFunction()
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
