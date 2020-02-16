a41632efd15b2bc38ab0cc8a5a08c4e6684e1d4e
JacOng17 <jacob_ong88@hotmail.com>
2020-02-17 11:12:37 +1300

    Deleted unused code.

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -60,58 +60,6 @@ jQuery(document).ready(function($){
         }
       }
     });
-    
-    
-//     // Hide Header on on scroll down
-//     var didScroll;
-//     var lastScrollTop = 0;
-//     var delta = 5;
-//     var navbarHeight = $('nav').outerHeight();
-
-//     $(window).scroll(function(event){
-//         didScroll = true;
-//     });
-
-//     setInterval(function() {
-//         if (didScroll) {
-//             hasScrolled();
-//             didScroll = false;
-//         }
-//     }, 250);
-
-//     function hasScrolled() {
-//         var st = $(this).scrollTop();
-        
-//         // Make sure they scroll more than delta
-//         if(Math.abs(lastScrollTop - st) <= delta)
-//             return;
-
-//         // If they scrolled down and are past the navbar, add class .nav-up.
-//         // This is necessary so you never see what is "behind" the navbar.
-//         if (st > lastScrollTop && st > navbarHeight){
-//             // Scroll Down            
-//             $('nav').removeClass('nav-down').addClass('nav-up'); 
-//             $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-           
-//         } else {
-//             // Scroll Up
-//             if(st + $(window).height() < $(document).height()) {               
-//                 $('nav').removeClass('nav-up').addClass('nav-down');
-//                 $('.nav-up, .nav-down').css('top', '0px');             
-//             }
-//         }
-
-//         lastScrollTop = st;
-//     }
-        
-//     $('.site-content').css('margin-top', $('header').outerHeight() + 'px');  
-    
-//     // spoilers
-//      $(document).on('click', '.spoiler', function() {
-//         $(this).removeClass('spoiler');
-//      });
-    
-//  });   
 
 // deferred style loading
 var loadDeferredStyles = function () {
