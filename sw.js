e09a3dc381b562b7a83c3f26e89a48616363465b
JacOng17 <JacOng17@users.noreply.github.com>
2021-02-22 14:21:32 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-self.addEventListener("install", function(event) {
+//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+//Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
 });
 
 var preLoad = function(){
-  console.log("Installing web app");
-  return caches.open("offline").then(function(cache) {
-    console.log("caching index and important routes");
-    return cache.addAll(["/", "/tag", "/about", "/index.html", "/facebook-feeds", "/categories", "/offline.html"]);
+  console.log('[PWA Builder] Install Event processing');
+  return caches.open('pwabuilder-offline').then(function(cache) {
+    console.log('[PWA Builder] Cached index and offline page during Install');
+      return cache.addAll([]);
   });
 };
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener('fetch', function(event) {
+  console.log('[PWA Builder] The service worker is serving the asset.');
   event.respondWith(checkResponse(event.request).catch(function() {
     return returnFromCache(event.request);
   }));
@@ -30,19 +34,19 @@ var checkResponse = function(request){
 };
 
 var addToCache = function(request){
-  return caches.open("offline").then(function (cache) {
+  return caches.open('pwabuilder-offline').then(function (cache) {
     return fetch(request).then(function (response) {
-      console.log(response.url + " was cached");
+      console.log('[PWA Builder] add page to offline'+response.url);
       return cache.put(request, response);
     });
   });
 };
 
 var returnFromCache = function(request){
-  return caches.open("offline").then(function (cache) {
+  return caches.open('pwabuilder-offline').then(function (cache) {
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
+       return cache.match('offline.html');
      } else {
        return matching;
      }
